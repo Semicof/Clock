9c7ee52a6d2b58a7f5b7c3ea372a426916e2ca21
Vuong <nhatvuongnguyen208@gmail.com>
2022-07-26 23:30:35 +0700

    Last commit

--- src/Clock.java
+++ src/Clock.java
@@ -6,19 +6,31 @@ import java.util.Calendar;
 public class Clock extends JFrame {
     static Calendar calendar;
     static SimpleDateFormat timeFormat;
+    static SimpleDateFormat dayFormat;
     static JLabel timeLabel;
+    static JLabel dayLabel;
     static String time;
+    static String day;
     Clock(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Cof's Clock");
         this.setLayout(new FlowLayout());
-        this.setSize(350,200);
+        this.setSize(350,180);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         timeFormat=new SimpleDateFormat("hh:mm:ss a");
         timeLabel=new JLabel();
+        timeLabel.setFont(new Font("Arial",Font.PLAIN,58));
+        timeLabel.setForeground(Color.lightGray);
+        timeLabel.setBackground(Color.black);
+        timeLabel.setOpaque(true);
+
+        dayLabel = new JLabel();
+        dayFormat=new SimpleDateFormat("E d-M-y");
+        dayLabel.setFont(new Font("Ink Free",Font.PLAIN,40));
 
         this.add(timeLabel);
+        this.add(dayLabel);
         this.setVisible(true);
         setTime();
     }
@@ -28,6 +40,9 @@ public class Clock extends JFrame {
         while(true) {
             time = timeFormat.format(Calendar.getInstance().getTime());
             timeLabel.setText(time);
+
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
             try{ Thread.sleep(1000);}
             catch (Exception e)
             {
